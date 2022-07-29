f9fc42812ae6ca7cb1000eef15fe67bb7612ba13
Jorge Frias <Jorge_Frias@epam.com>
2022-07-29 09:30:47 -0500

    Change of bff url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   product: 'https://hih14h2qbi.execute-api.us-east-1.amazonaws.com',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  bff: 'https://hih14h2qbi.execute-api.us-east-1.amazonaws.com',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
