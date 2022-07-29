e98e7dc000c84408e55b19e9e0651705f193a99c
Jorge Frias <Jorge_Frias@epam.com>
2022-07-29 13:59:02 -0500

    Adding new urls

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 
 const API_PATHS = {
-  product: 'https://7ffzx02d8f.execute-api.us-east-1.amazonaws.com',
+  product: 'https://jewv4vzw6b.execute-api.us-east-1.amazonaws.com',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://7ffzx02d8f.execute-api.us-east-1.amazonaws.com',
+  bff: 'https://jewv4vzw6b.execute-api.us-east-1.amazonaws.com',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
