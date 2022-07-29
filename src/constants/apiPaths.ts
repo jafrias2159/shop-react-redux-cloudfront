0793efa9a11d2405cc5b001c6ed14ab496c43c5b
Jorge Frias <Jorge_Frias@epam.com>
2022-07-29 12:01:10 -0500

    Adding new api pahs

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 
 const API_PATHS = {
-  product: 'https://hih14h2qbi.execute-api.us-east-1.amazonaws.com',
+  product: 'https://7ffzx02d8f.execute-api.us-east-1.amazonaws.com',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://hih14h2qbi.execute-api.us-east-1.amazonaws.com',
+  bff: 'https://7ffzx02d8f.execute-api.us-east-1.amazonaws.com',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
